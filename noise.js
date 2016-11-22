a75491898d2a7163cc4cdf26407f2d729a7b6927
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-21 16:56:12 -0800

    click - sound working

--- noise.js
+++ noise.js
@@ -1,3 +1,24 @@
+// create an audio object to be able to call play on...
+
+var audioSounds = ["cAudio", "dAudio", "eAudio", "fAudio", "gAudio", "aAudio", "bAudio"];
+// When cnote is clicked cAudio is played...
+
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event){
+    if ($(this).html() === 'c'){
+      $('#cAudio')[0].play();
+    } else if ($(this).html() === 'd'){
+      $('#dAudio')[0].play();
+    } else if ($(this).html() === 'e'){
+      $('#eAudio')[0].play();
+    } else if ($(this).html() === 'f'){
+      $('#fAudio')[0].play();
+    } else if ($(this).html() === 'g'){
+      $('#gAudio')[0].play();
+    } else if ($(this).html() === 'a'){
+      $('#aAudio')[0].play();
+    } else if ($(this).html() === 'b'){
+      $('#bAudio')[0].play();
+    }
+  });
 });
